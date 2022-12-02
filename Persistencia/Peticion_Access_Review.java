a222941e851ce616d2fdaf48dbf76d2266721bc3
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2022-12-02 22:13:46 +0100

    Create Peticion_Access_Review.java

---
+++ Persistencia/Peticion_Access_Review.java
@@ -0,0 +1,30 @@
+package Dominio;
+
+public class Peticion_Access_Review {
+
+	private String review;
+
+	public Peticion_Access_Review() {
+		// TODO - implement Peticion_Access_Review.Peticion_Access_Review
+		
+	}
+
+	private void getReview() {
+		// TODO - implement Peticion_Access_Review.getReview
+		
+	}
+
+	/**
+	 * 
+	 * @param login
+	 */
+	public String setAttribute(int login) {
+		// TODO - implement Peticion_Access_Review.setAttribute
+		
+	}
+
+	public void getReview() {
+		// TODO - implement Peticion_Access_Review.getReview
+		
+	}
+}
