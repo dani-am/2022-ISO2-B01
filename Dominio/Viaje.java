bcb2ab63a0d9b71acceff07d32cc186290a50fa5
RodrigoGomez2 <91531538+RodrigoGomez2@users.noreply.github.com>
2022-12-09 21:02:27 +0100

    Finalización Implementación Iteración 8(peregrino)

---
+++ Dominio/Viaje.java
@@ -0,0 +1,28 @@
+package Dominio;
+
+public class Viaje {
+
+	private int ID_Viaje;
+	private double Fecha_Viaje;
+
+	public boolean Añadir_Viaje() {
+		// TODO - implement Viaje.Añadir_Viaje
+		
+	}
+
+	public boolean Modificar_Viaje() {
+		// TODO - implement Viaje.Modificar_Viaje
+		
+	}
+
+	public boolean Eliminar_Viaje() {
+		// TODO - implement Viaje.Eliminar_Viaje
+		
+	}
+
+	public boolean Buscar_Viaje() {
+		// TODO - implement Viaje.Buscar_Viaje
+		
+	}
+
+}
