934041d4a03d4e785e57fca5a21854e46fdc334d
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2022-12-13 20:14:39 +0100

    Update Aniadir_comentario.java

--- Dominio/Aniadir_comentario.java
+++ Dominio/Aniadir_comentario.java
@@ -3,6 +3,9 @@ package Dominio;
 public class Aniadir_comentario{
 
 	private String Comentario;
+	
+	public Comentario(){
+	}
 
 	public String getComentario() {
 		// TODO - implement Aniadir_comentario.getComentario
