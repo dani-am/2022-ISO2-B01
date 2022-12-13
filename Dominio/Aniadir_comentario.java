7f4ef8d6636d2112af707e2d8b440e5b5339730e
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2022-12-13 19:56:53 +0100

    Update and rename Analisis_de_reseñas.java to Aniadir_comentario.java

---
+++ Dominio/Aniadir_comentario.java
@@ -0,0 +1,19 @@
+package Dominio;
+
+public class Aniadir_comentario{
+
+	private String Comentario;
+
+	public String getComentario() {
+		// TODO - implement Aniadir_comentario.getComentario
+		
+	}
+	
+	public String setComentario() {
+		// TODO - implement Aniadir_comentario.setComentario
+		// implement censor level
+		
+	}
+
+
+}
