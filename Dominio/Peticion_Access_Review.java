17b35a0d98f5812459c2fb37ea1a674d2546d2f4
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2022-12-13 20:08:12 +0100

    Update and rename Nuevo documento de texto.txt to Peticion_Access_Review.java

---
+++ Dominio/Peticion_Access_Review.java
@@ -0,0 +1,19 @@
+package Dominio;
+
+public class Peticion_Access_Review {
+
+	public boolean añadirDatos() {
+		// TODO - implement Gestionar_Datos_peregrinos.añadirDatos
+		
+	}
+
+	public boolean modificarDatos() {
+		// TODO - implement Gestionar_Datos_peregrinos.modificarDatos
+		
+	}
+
+	public boolean eliminarDatos() {
+		// TODO - implement Gestionar_Datos_peregrinos.eliminarDatos
+		
+	}
+
