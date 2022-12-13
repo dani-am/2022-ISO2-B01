2f39437fef17a379268a8a14691a95145d97db8e
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2022-12-13 19:45:42 +0100

    Update and rename Gestionar_Datos_peregrinos.java to Analisis_de_reseñas.java

---
+++ Dominio/Analisis_de_reseñas.java
@@ -0,0 +1,22 @@
+package Dominio;
+
+public class Analisis_de_reseñas {
+	
+	private double Nota;
+
+	public String getReseñas() {
+		// TODO - implement Analisis_de_reseñas.getReseñas
+		
+	}
+
+	public double getNota() {
+		// TODO - implement Analisis_de_reseñas.getNota
+		
+	}
+
+	public void setNota() {
+		// TODO - implement Analisis_de_reseñas.setNota()
+		
+	}
+
+}
