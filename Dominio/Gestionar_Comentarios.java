6b53170cd45ca6824f54eb56d1bc7e1cd719958a
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2022-12-16 01:11:52 +0100

    Create Gestionar_Comentarios.java

---
+++ Dominio/Gestionar_Comentarios.java
@@ -0,0 +1,16 @@
+package Dominio;
+
+public class Gestionar_Comentarios {
+
+	public boolean añadirComentario() {
+		// TODO - implement Gestionar_Comentarios.añadirComentario
+		
+	}
+
+	public boolean censurarComentario() {
+		// TODO - implement Gestionar_Comentarios.censurarComentario
+		
+	}
+
+
+}
